9d4dccd01e5166d3a6a59c212ac06f82c8a9acb5
Omkatkar007 <omkatkar718@gmail.com>
2026-02-11 21:40:28 +0530

    Add files via upload

---
+++ variables&data_types.py
@@ -0,0 +1,42 @@
+#variables 
+
+var = "container"
+age = 23
+print(var,age)
+
+#data_types
+
+int = 23  #1.int(whole no,s not decimals)
+
+float = 3.45 #2. decimals
+
+str = "text" #3. anything in double/single quote
+
+bool_torf = True #4. bool true or false conditions
+
+list = [ 90 , 45 , "om" , "any"] #5. used in [] 
+
+tuple = (10 , "immutable") #6. used in () immutable
+
+dict = {"name" : "om",
+        "age" : 34,
+        "city" : "pune"
+        }                  #7. used in {} and key pair values
+print(type())
+
+'''python automatically detects data type'''
+
+"""type conversion"""
+
+x = 10 
+y = float(x)  #int to float
+
+x = 3.45 
+y = int(x)   #float to int 
+
+x = 10 
+y = str(x)  #int to string 
+
+
+
+
